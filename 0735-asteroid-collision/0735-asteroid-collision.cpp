a1eb2e94509fbc97d22c50538d008ea2f8ee4074
MadhavJee <150125017+MadhavJee@users.noreply.github.com>
2026-04-23 22:32:26 +0530

    Time: 3 ms (28.68%), Space: 21.4 MB (96.86%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for(int a : asteroids){
+            while(!st.empty() && a < 0 && st.back() > 0){
+                if(st.back() < -a){
+                    st.pop_back();
+                }
+                else if(st.back() == -a){
+                    st.pop_back();
+                    a = 0;
+                    break;
+                }
+                else{
+                    a = 0;
+                    break;
+                }
+            }
+            if(a != 0) st.push_back(a);
+        }
+        return st;
+    }
+};
