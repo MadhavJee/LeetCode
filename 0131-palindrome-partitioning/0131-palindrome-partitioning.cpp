eca3cafa9b8752c03d06e6ed5ef9fee0d29c7d4d
MadhavJee <150125017+MadhavJee@users.noreply.github.com>
2026-05-13 12:36:10 +0530

    Time: 63 ms (25.45%), Space: 79.9 MB (27.72%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    bool isPal(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
+    }
+    void solve(int idx, string &s, vector<string> &path) {
+        if (idx == s.size()) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = idx; i < s.size(); i++) {
+            if (isPal(s, idx, i)) {
+                path.push_back(s.substr(idx, i - idx + 1));
+                solve(i + 1, s, path);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> path;
+        solve(0, s, path);
+        return ans;
+    }
+};
