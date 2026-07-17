bb0b318b7855dcb2a5ca11f6f7adfc3d7c56c6d0
MadhavJee <150125017+MadhavJee@users.noreply.github.com>
2026-07-17 14:29:04 +0530

    Time: 0 ms (100%), Space: 12.1 MB (16.51%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(int num:arr){
+            mp[num]++;
+        }
+        unordered_set<int>st;
+        for(auto x:mp){
+            if(st.count(x.second))return false;
+            st.insert(x.second);
+        }
+        return true;
+    }
+};
