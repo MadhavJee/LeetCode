bf61d0151bd91ddf3de8c691152fbfa42343032a
MadhavJee <150125017+MadhavJee@users.noreply.github.com>
2026-08-03 15:46:44 +0530

    Time: 53 ms (21.69%), Space: 18.2 MB (93.65%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,39 @@
+class DSU{
+    public:
+    vector<int>parent;
+    DSU(int n){
+        parent.resize(n+1);
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int x) {
+        if (parent[x] == x)return x;
+        return parent[x] = find(parent[x]);
+    }
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px != py)parent[px] = py;
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        DSU dsu(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] ||
+                    stones[i][1] == stones[j][1]) {
+                    dsu.unite(i, j);
+                }
+            }
+        }
+        int com = 0;
+        for (int i = 0; i < n; i++) {
+            if (dsu.find(i) == i)com++;
+        }
+        return n - com;
+    }
+};
